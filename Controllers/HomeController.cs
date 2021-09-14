290b71a2f3ed119d5e1f180885734a59b46eb120
Mariam AIT MOUSSA <mariam15aitmoussa@gmail.com>
2021-09-14 16:58:52 +0100

    adding project's files

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,65 @@
+﻿
+using App.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace App.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult LoginRespo()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult LoginRespo(Responsable model, string returnUrl)
+        {
+            AuthentificationEntities1 db = new AuthentificationEntities1();
+            var dataItem = db.Responsables.Where(x => x.UserName == model.UserName && x.PassWord == model.PassWord).First();
+            if (dataItem != null)
+            {
+                FormsAuthentication.SetAuthCookie(dataItem.UserName, false);
+                if (Url.IsLocalUrl(returnUrl) && returnUrl.Length > 1 && returnUrl.StartsWith("/")
+                         && !returnUrl.StartsWith("//") && !returnUrl.StartsWith("/\\"))
+                {
+                    return Redirect(returnUrl);
+                }
+                else
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("", "Invalid user/pass");
+                return View();
+            }
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        
+
+
+    }
+}
